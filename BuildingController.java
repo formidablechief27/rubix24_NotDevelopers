cda9c9901e526d10b28772b47d0f35d9a1c2849e
formidablechief27 <122151503+formidablechief27@users.noreply.github.com>
2024-01-19 10:30:46 +0530

    nice

--- BuildingController.java
+++ BuildingController.java
@@ -1,5 +1,15 @@
 package com.example.disaster;
 
-public class BuildingController {
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
+public class BuildingController {
+	
+	@GetMapping("/get-building")
+	public String ret(Model model) {
+		return "building.html";
+	}
+	
 }
